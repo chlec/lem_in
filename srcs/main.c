28e36ca7b0d08315b2c06fe8cd31593cf8cfc27d
Charles LECALIER <clecalie@e2r7p3.42.fr>
2018-02-15 16:01:39 +0100

    Ajout un peu de propre

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,36 @@
 #include "lem_in.h"
 
+void	handle_command(t_env *env, char *line)
+{
+	char	**temp;
+	
+	if (!ft_strcmp(line, "##start"))
+	{
+		get_next_line(0, &line);
+		temp = ft_strsplit(line, ' ');
+		env->start = (t_room*)malloc(sizeof(t_room));
+		env->start->name = temp[0];
+		env->start->x = ft_atoi(temp[1]);
+		env->start->y = ft_atoi(temp[2]);
+		env->start->next = NULL;
+		//	ft_strdel(&temp);	
+		ft_strdel(&line);
+	}
+	else if (!ft_strcmp(line, "##end"))
+	{
+		get_next_line(0, &line);
+		temp = ft_strsplit(line, ' ');
+		env->end = (t_room*)malloc(sizeof(t_room));
+		env->end->name = temp[0];
+		env->end->y = ft_atoi(temp[1]);
+		env->end->y = ft_atoi(temp[2]);
+		env->end->next = NULL;
+		//	ft_strdel(&temp);	
+		ft_strdel(&line);
+	}
+
+}
+
 int		main(void)
 {
 	int		ret;
@@ -18,6 +49,7 @@ int		main(void)
 	{
 		if (i == 0)
 			env->nb_ant = ft_atoi(line);
+		//ROOM
 		if (ft_strlen(line) > 1 && line[1] == ' ')
 		{
 			if (!(room = (t_room*)malloc(sizeof(room))))
@@ -30,30 +62,8 @@ int		main(void)
 			room = room->next;
 			//ft_strdel(&temp);
 		}
-		else if (!ft_strcmp(line, "##start"))
-		{
-			ft_strdel(&line);	
-			get_next_line(0, &line);
-			temp = ft_strsplit(line, ' ');
-			env->start = (t_room*)malloc(sizeof(t_room));
-			env->start->name = temp[0];
-			env->start->x = ft_atoi(temp[1]);
-			env->start->y = ft_atoi(temp[2]);
-			env->start->next = NULL;
-		//	ft_strdel(&temp);	
-		}
-		else if (!ft_strcmp(line, "##end"))
-		{
-			ft_strdel(&line);	
-			get_next_line(0, &line);
-			temp = ft_strsplit(line, ' ');
-			env->end = (t_room*)malloc(sizeof(t_room));
-			env->end->name = temp[0];
-			env->end->y = ft_atoi(temp[1]);
-			env->end->y = ft_atoi(temp[2]);
-			env->end->next = NULL;
-		//	ft_strdel(&temp);	
-		}
+		else if (line[0] == '#')
+			handle_command(env, line);
 		i++;
 		ft_strdel(&line);	
 	}
