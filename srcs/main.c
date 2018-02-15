8ec5fbbe34c0ca4a37aa8e7eb625f67db7f8ac48
Charles LECALIER <clecalie@e2r7p3.42.fr>
2018-02-15 15:58:09 +0100

    segv regle

--- srcs/main.c
+++ srcs/main.c
@@ -12,42 +12,43 @@ int		main(void)
 	line = NULL;
 	i = 0;
 	room = NULL;
-/*	if (!(room = (t_room*)malloc(sizeof(room))))
+	if (!(env = (t_env*)malloc(sizeof(env))))
 		return (0);
-*/	if (!(env = (t_env*)malloc(sizeof(env))))
-		return (0);
-	while ((ret = get_next_line(0, &line)))
+	while ((ret = get_next_line(0, &line)) > 0)
 	{
 		if (i == 0)
 			env->nb_ant = ft_atoi(line);
-		if (line[1] == ' ')
+		if (ft_strlen(line) > 1 && line[1] == ' ')
 		{
 			if (!(room = (t_room*)malloc(sizeof(room))))
 				return (0);
 			temp = ft_strsplit(line, ' ');
-			ft_strcpy(room->name, temp[0]);
+			room->name = temp[0];
 			room->x = ft_atoi(temp[1]);
 			room->y = ft_atoi(temp[2]);
 			room->next = NULL;
 			room = room->next;
 			//ft_strdel(&temp);
 		}
-		if (!ft_strcmp(line, "##start"))
+		else if (!ft_strcmp(line, "##start"))
 		{
 			ft_strdel(&line);	
 			get_next_line(0, &line);
 			temp = ft_strsplit(line, ' ');
-			ft_strcpy(env->start->name, temp[0]);
+			env->start = (t_room*)malloc(sizeof(t_room));
+			env->start->name = temp[0];
 			env->start->x = ft_atoi(temp[1]);
 			env->start->y = ft_atoi(temp[2]);
+			env->start->next = NULL;
 		//	ft_strdel(&temp);	
 		}
-		if (!ft_strcmp(line, "##end"))
+		else if (!ft_strcmp(line, "##end"))
 		{
 			ft_strdel(&line);	
 			get_next_line(0, &line);
 			temp = ft_strsplit(line, ' ');
-			ft_strcpy(env->end->name, temp[0]);
+			env->end = (t_room*)malloc(sizeof(t_room));
+			env->end->name = temp[0];
 			env->end->y = ft_atoi(temp[1]);
 			env->end->y = ft_atoi(temp[2]);
 			env->end->next = NULL;
