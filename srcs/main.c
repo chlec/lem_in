dc07691ac6851d8d809d0f524307073a45cbbc91
mdaunois <mathieudaunois@gmail.com>
2018-02-15 14:58:53 +0100

    init de start et end mais a verifier

--- srcs/main.c
+++ srcs/main.c
@@ -5,6 +5,7 @@ int		main(void)
 	int		ret;
 	char	*line;
 	t_env	*env;
+	char	**temp;
 	int		i;
 
 	line = NULL;
@@ -15,6 +16,23 @@ int		main(void)
 	{
 		if (i == 0)
 			env->nb_ant = ft_atoi(line);
+		if (!ft_strcmp(line, ##start))
+		{
+			get_next_line(0, &line);
+			temp = ft_strsplit(line, ' ');
+			ft_strcpy(env->start->name, temp[0]);
+			env->start.x = ft_atoi(temp[1]);
+			env->start.x = ft_atoi(temp[2]);
+		}
+		if (!ft_strcmp(line, ##end))
+		{
+			get_next_line(0, &line);
+			temp = ft_strsplit(line, ' ');
+			ft_strcpy(env->end->name, temp[0]);
+			env->end.y = ft_atoi(temp[1]);
+			env->end.y = ft_atoi(temp[2]);
+			env->end->next = NULL;
+		}
 		i++;
 		ft_strdel(&line);	
 	}
