55c124de1ebf36e90b9bc5bca696f55d62c73f9f
Charles LECALIER <clecalie@e2r7p4.42.fr>
2018-02-16 13:47:19 +0100

    petit chemin pas complet

--- srcs/main.c
+++ srcs/main.c
@@ -3,7 +3,7 @@
 void	handle_command(t_env *env, char *line)
 {
 	char	**temp;
-	
+
 	if (!ft_strcmp(line, "##start"))
 	{
 		get_next_line(0, &line);
@@ -31,14 +31,25 @@ void	handle_command(t_env *env, char *line)
 
 }
 
+t_path	*copy_maillon(t_path *p)
+{
+	t_path	*ret;
+
+	ret = malloc(sizeof(t_path));
+	ret->room = p->room;
+	ret->len = p->len;
+	return (ret);	
+}
+
 void	cree_path(t_env *env)
 {
 	t_path	*path;
 	t_list	*PIPE;
-	t_list	*ROOM;
+//	t_list	*ROOM;
 	t_room	*room1;
 	t_room	*room2;
 	t_pipe	*pipe2;
+	t_path	*path_temp;
 
 	path = (t_path*)malloc(sizeof(t_path));
 	path->room = NULL;
@@ -46,29 +57,46 @@ void	cree_path(t_env *env)
 	PIPE = env->head_pipe;
 	while (PIPE)
 	{
-		printf("pipe...\n");
 		pipe2 = (t_pipe*)(PIPE->content);
-		ROOM = env->head_room;
-		while (ROOM)
-		{
-			room1 = (t_room*)ROOM->content;
-			if (room1->name == pipe2->left->name || room1->name == pipe2->right->name)
+		path_temp = copy_maillon(path);
+		room1 = NULL;
+		//Idee: On parcourt path_temp jusquau dernier et apres on check si on a une suite
+		while (path_temp->room)
+		{	
+			if (path_temp->room->next == NULL)
 			{
-				while (path->room)
+				room1 = (t_room*)path_temp->room->content;
+				break ;
+			}
+			path_temp->room = path_temp->room->next;
+		}
+		if (room1)
+		{
+			printf("on check room %s...\n", room1->name);
+			if (ft_strequ(room1->name, pipe2->left->name) || ft_strequ(room1->name, pipe2->right->name))
+			{
+				path_temp = copy_maillon(path);
+				while (path_temp->room)
 				{
-					room2 = (t_room*)(path->room->content);
-					if (room2->name == room1->name)
+					room2 = (t_room*)(path_temp->room->content);
+					if (ft_strequ(room2->name, room1->name))
 						break;
-					path->room = path->room->next;
+					path_temp->room = path_temp->room->next;
 				}
 				//On ajoute la room2 au path
-				printf("on ajoute a la suite\n");
-				ft_list_push_back(&(path->room), room1->name == pipe2->left->name ? pipe2->right : pipe2->left, sizeof(t_room));
+				ft_list_push_back(&(path->room), ft_strequ(room1->name, pipe2->left->name) ? pipe2->right : pipe2->left, sizeof(t_room));
+				printf("on ajoute a la suite la room %s\n", ft_strequ(room1->name, pipe2->left->name) ? pipe2->right->name : pipe2->left->name);
 				path->len++;
 				break;
 			}
-			ROOM = ROOM->next;
-		}	
+		}
+		else
+		{
+			printf("on ajoute %s \n", pipe2->left->name);
+			ft_list_push_back(&(path->room), pipe2->left, sizeof(t_room));
+			printf("on ajoute %s \n", pipe2->right->name);
+			ft_list_push_back(&(path->room), pipe2->right, sizeof(t_room));
+		}
 		PIPE = PIPE->next;
 	}
 	while (path->room)
@@ -81,96 +109,96 @@ void	cree_path(t_env *env)
 
 int		main(void)
 {
-    int		ret;
-    char    *line;
-    t_env	*env;
-    t_list	*head_temp;
-    t_list	*head_path;
-    t_room	*room;
-    char    **temp;
+	int		ret;
+	char    *line;
+	t_env	*env;
+	t_list	*head_temp;
+	t_list	*head_path;
+	t_room	*room;
+	char    **temp;
 	t_path	*path;
 	t_pipe	*pipe;
-   
-   	path = NULL;	
-    line = NULL;
-    head_path = NULL;
-    room = NULL;
-    if (!(env = (t_env*)malloc(sizeof(t_env))))
-        return (0);
+
+	path = NULL;	
+	line = NULL;
+	head_path = NULL;
+	room = NULL;
+	if (!(env = (t_env*)malloc(sizeof(t_env))))
+		return (0);
 	env->head_room = NULL;
 	env->head_pipe = NULL;
 	pipe = NULL;
 	get_next_line(0, &line);
-    env->nb_ant = ft_atoi(line);
-    while ((ret = get_next_line(0, &line)) > 0)
-    {
-        //ROOM
-        if (ft_strlen(line) > 1 && line[1] == ' ')
-        {
-            if (!(room = (t_room*)malloc(sizeof(t_room))))
-                return (0);
-            temp = ft_strsplit(line, ' ');
-            room->name = temp[0];
-            room->x = ft_atoi(temp[1]);
-            room->y = ft_atoi(temp[2]);
-            ft_list_push_back(&(env->head_room), room, sizeof(t_room));
-            room = NULL;
-            //ft_strdel(&temp);
-        }
-        else if (line[0] == '#')
-            handle_command(env, line);
-        else if (line[1] == '-')
-        {
-			pipe = (t_pipe*)malloc(sizeof(t_pipe));
-            temp = ft_strsplit(line, '-');
-            printf("%s     %s\n", temp[0], temp[1]);
-            head_temp = env->head_room;
-            while (head_temp)
-            {
-				//room = contenu de head temp == head_room
-                room = (t_room*)(head_temp->content);
-                if (!ft_strcmp(room->name, temp[0]))
-                {
-                    pipe->left = room;
-                    printf("room de depart = %s\n", pipe->left->name);
-                }
-                head_temp = head_temp->next;
-            }
-            head_temp = env->head_room;
-            while (head_temp)
-            {
-				//room = contenu de head temp == head_room
-                room = (t_room*)(head_temp->content);
-                if (!ft_strcmp(room->name, temp[1]))
-                {
-                    pipe->right = room;
-                    printf("room de depart = %s\n", pipe->right->name);
-                }
-                head_temp = head_temp->next;
-            }
-			ft_list_push_back(&(env->head_pipe), pipe, sizeof(t_pipe));
-        }
-        ft_strdel(&line);
-
-    }
-    /*while (env->head_room)
-    {
-        room = (t_room*)(env->head_room->content);
-        printf("nom de room = %s\n", room->name);
-        env->head_room = env->head_room->next;
-    }*/
-/*	while (env->head_pipe)
+	env->nb_ant = ft_atoi(line);
+	while ((ret = get_next_line(0, &line)) > 0)
 	{
+		//ROOM
+		if (ft_strlen(line) > 1 && line[1] == ' ')
+		{
+			if (!(room = (t_room*)malloc(sizeof(t_room))))
+				return (0);
+			temp = ft_strsplit(line, ' ');
+			room->name = temp[0];
+			room->x = ft_atoi(temp[1]);
+			room->y = ft_atoi(temp[2]);
+			ft_list_push_back(&(env->head_room), room, sizeof(t_room));
+			room = NULL;
+			//ft_strdel(&temp);
+		}
+		else if (line[0] == '#')
+			handle_command(env, line);
+		else if (line[1] == '-')
+		{
+			pipe = (t_pipe*)malloc(sizeof(t_pipe));
+			temp = ft_strsplit(line, '-');
+			printf("%s     %s\n", temp[0], temp[1]);
+			head_temp = env->head_room;
+			while (head_temp)
+			{
+				//room = contenu de head temp == head_room
+				room = (t_room*)(head_temp->content);
+				if (!ft_strcmp(room->name, temp[0]))
+				{
+					pipe->left = room;
+					printf("room de depart = %s\n", pipe->left->name);
+				}
+				head_temp = head_temp->next;
+			}
+			head_temp = env->head_room;
+			while (head_temp)
+			{
+				//room = contenu de head temp == head_room
+				room = (t_room*)(head_temp->content);
+				if (!ft_strcmp(room->name, temp[1]))
+				{
+					pipe->right = room;
+					printf("room de depart = %s\n", pipe->right->name);
+				}
+				head_temp = head_temp->next;
+			}
+			ft_list_push_back(&(env->head_pipe), pipe, sizeof(t_pipe));
+		}
+		ft_strdel(&line);
+
+	}
+	/*while (env->head_room)
+	  {
+	  room = (t_room*)(env->head_room->content);
+	  printf("nom de room = %s\n", room->name);
+	  env->head_room = env->head_room->next;
+	  }*/
+	/*	while (env->head_pipe)
+		{
 		pipe = (t_pipe*)(env->head_pipe->content);
 		if (pipe->left && pipe->right)
 		{
-       		 printf("nom de path left = %s\t", pipe->left->name);
-       	 	printf("nom de path right = %s\n", pipe->right->name);
+		printf("nom de path left = %s\t", pipe->left->name);
+		printf("nom de path right = %s\n", pipe->right->name);
 		}
 		env->head_pipe = env->head_pipe->next;
-	}*/
+		}*/
 	cree_path(env);
-    /*printf("nom de start = %s x = %d y = %d\n", env->start->name, env->start->x, env->start->y);
-    printf("nom de end = %s x = %d y = %d\n", env->end->name, env->end->x, env->end->y);*/
-    return (0);
+	/*printf("nom de start = %s x = %d y = %d\n", env->start->name, env->start->x, env->start->y);
+	  printf("nom de end = %s x = %d y = %d\n", env->end->name, env->end->x, env->end->y);*/
+	return (0);
 }
