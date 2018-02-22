4cc46434d357c3235bf696658f8aabeb96490fa6
mdaunois <mathieudaunois@gmail.com>
2018-02-22 13:20:45 +0100

    init_ant fini

--- srcs/move_ant.c
+++ srcs/move_ant.c
@@ -6,7 +6,7 @@
 /*   By: clecalie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 15:53:28 by clecalie          #+#    #+#             */
-/*   Updated: 2018/02/22 12:21:49 by mdaunois         ###   ########.fr       */
+/*   Updated: 2018/02/22 13:19:48 by mdaunois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,35 @@
 void	init_ant(t_env *env)
 {
 	t_ant	*l_ant;
-	t_list	*save_head;
 	int		i;
 
+	l_ant = NULL;
 	i = 0;
-	save_head = env->head_ant;
 	while (i < env->nb_ant)
 	{
-		l_ant = (t_ant*)malloc(sizeof(t_ant));
+		if(!(l_ant = (t_ant*)malloc(sizeof(t_ant))))
+			return ;
 		l_ant->num = i + 1;
 		l_ant->position = env->start->name;
 		ft_list_push_back(&(env->head_ant), l_ant, sizeof(t_ant));
-		printf("L%d-%s\n", l_ant->num, l_ant->position);
 		l_ant = NULL;
 		i++;
-//		env->head_ant = env->head_ant->next;
 	}
-	env->head_ant = save_head;
 }
 
 
 void	print_ant(t_env *env)
 {
 	t_ant	*l_ant;
+	t_list	*head_temp;
 
-	while (env->head_ant)
+	head_temp = env->head_ant;
+
+	while (head_temp)
 	{
-		l_ant = (t_ant*)env->head_ant;
+		l_ant = (t_ant*)(head_temp->content);
 		printf("L%d-%s\n", l_ant->num, l_ant->position);
-		env->head_ant = env->head_ant->next;
+		head_temp = head_temp->next;
 	}
 }
 
