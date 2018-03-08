a7cfed654602383ab853fb15ed1e230f02361d28
mdaunois <mathieudaunois@gmail.com>
2018-03-08 17:21:27 +0100

    norme ant

--- srcs/move_ant.c
+++ srcs/move_ant.c
@@ -6,7 +6,7 @@
 /*   By: clecalie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/21 15:53:28 by clecalie          #+#    #+#             */
-/*   Updated: 2018/03/08 12:59:30 by mdaunois         ###   ########.fr       */
+/*   Updated: 2018/03/08 17:15:11 by mdaunois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,12 @@ void	init_ant(t_env *env)
 	}
 }
 
-
 void	find_ant(t_env *env, char *pos_init, char *new_pos)
 {
 	t_ant	*l_ant;
 	t_list	*head_temp;
 
 	head_temp = env->head_ant;
-
 	while (head_temp)
 	{
 		l_ant = (t_ant*)(head_temp->content);
@@ -49,6 +47,25 @@ void	find_ant(t_env *env, char *pos_init, char *new_pos)
 			return ;
 		}
 		head_temp = head_temp->next;
+	}
+}
+
+void	del_ant(t_env *env, t_list *head_temp, t_list *head, t_ant *l_ant)
+{
+	if (head == NULL || env->head_ant == head_temp)
+	{
+		free(l_ant);
+		l_ant = NULL;
+		free(env->head_ant);
+		env->head_ant = head_temp->next;
+	}
+	else
+	{
+		free(l_ant);
+		l_ant = NULL;
+		head->next = head_temp->next;
+		free(head_temp);
+		head_temp = head;
 	}
 }
 
@@ -63,44 +80,11 @@ void	print_antv2(t_env *env)
 	while (head_temp)
 	{
 		l_ant = (t_ant*)(head_temp->content);
-		if (ft_strcmp(l_ant->position, env->start->name))// && ft_strcmp(l_ant->position, env->end->name))
+		if (ft_strcmp(l_ant->position, env->start->name))
 			printf("L%d-%s ", l_ant->num, l_ant->position);
-		//En gros si une fourmi arrive a 0, on la supprime de la chaine
 		if (!ft_strcmp(l_ant->position, env->end->name))
-		{
-		//	printf("la fourmie %d et arriver  ||  ", l_ant->num);
-			if (head == NULL || env->head_ant == head_temp)
-			{
-				free(l_ant);
-				l_ant = NULL;
-				free(env->head_ant);
-				env->head_ant = head_temp->next;
-			}
-			else
-			{
-				free(l_ant);
-				l_ant = NULL;
-				head->next = head_temp->next;
-				free(head_temp);
-				head_temp = head;
-			}
-		}
+			del_ant(env, head_temp, head, l_ant);
 		head = head_temp;
-		head_temp = head_temp->next;
-	}
-	printf("\n");
-}
-
-void	print_ant(t_env *env)
-{
-	t_ant	*l_ant;
-	t_list	*head_temp;
-
-	head_temp = env->head_ant;
-	while (head_temp)
-	{
-		l_ant = (t_ant*)(head_temp->content);
-		printf("L%d-%s ", l_ant->num, l_ant->position);
 		head_temp = head_temp->next;
 	}
 	printf("\n");
@@ -108,7 +92,6 @@ void	print_ant(t_env *env)
 
 void	ant_is_moving(t_env *env, int len, t_room *room, t_room *room_after)
 {
-//	printf("room %s a %d,room %s a %d\n",room->name, room->ant,room_after->name, room_after->ant);
 	if (!ft_strcmp(room_after->name, env->start->name))
 	{
 		room_after->ant = env->start->ant;
@@ -116,20 +99,15 @@ void	ant_is_moving(t_env *env, int len, t_room *room, t_room *room_after)
 			return ;
 	}
 	if (!ft_strcmp(room->name, env->end->name))
-	{
-		room->ant = env->end->ant; 	
-	}
+		room->ant = env->end->ant;
 	if (!ft_strcmp(room->name, env->end->name) && room_after->ant > 0)
 	{
-		//printf("fourmis qui viens de la room %s\n %d\n", room_after->name, room_after->ant);
 		find_ant(env, room_after->name, room->name);
-		env->end->ant = env->end->ant + 1; 
+		env->end->ant = env->end->ant + 1;
 		room->ant = room->ant + 1;
 		room_after->ant = room_after->ant - 1;
 		if (!ft_strcmp(room_after->name, env->start->name))
-		{
-			env->start->ant = env->start->ant - 1; 	
-		}
+			env->start->ant = env->start->ant - 1;
 	}
 	else if (room->ant == 0 && room_after->ant > 0)
 	{
@@ -137,15 +115,10 @@ void	ant_is_moving(t_env *env, int len, t_room *room, t_room *room_after)
 		room->ant = room->ant + 1;
 		room_after->ant = room_after->ant - 1;
 		if (!ft_strcmp(room_after->name, env->start->name))
-		{
-			env->start->ant = env->start->ant - 1; 	
-		}
+			env->start->ant = env->start->ant - 1;
 	}
-//	printf("room %s a %d,room %s a %d\n",room->name, room->ant, room_after->name, room_after->ant);
-//	printf("\n");
 }
 
-/*il faut faire touts les chemins possible quant il faut, et afficher la fourmie qui est a la room end*/
 void	move_ant(t_env *env)
 {
 	t_list	*head;
@@ -154,9 +127,7 @@ void	move_ant(t_env *env)
 
 	path = NULL;
 	head = NULL;
-	head_p = NULL;
 	init_ant(env);
-//	print_ant(env);
 	while (env->end->ant < env->nb_ant)
 	{
 		head_p = env->head_path;
@@ -166,20 +137,13 @@ void	move_ant(t_env *env)
 			head = path->room;
 			while (path->room->next)
 			{
-				ant_is_moving(env, path->len,  path->room->content, path->room->next->content);
+				ant_is_moving(env, path->len, path->room->content,
+	path->room->next->content);
 				path->room = path->room->next;
 			}
 			path->room = head;
-//			usleep(10000);
-//			print_antv2(env);
 			head_p = head_p->next;
 		}
 		print_antv2(env);
-//		printf("fourmis fini = %d fourmis total = %d\n", env->end->ant, env->nb_ant);
 	}
 }
-
-
-
-
-
